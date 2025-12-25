e301b3bf3a89e6d3cfebe07a3a518a15b692f935
jattu8602 <chaurasiyajatin68@gmail.com>
2025-12-25 14:07:57 +0530

    feat: Integrate Google Services into Android build and add login/home screen imports to splash screen.

--- lib/src/screens/auth/splash_screen.dart
+++ lib/src/screens/auth/splash_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../providers/auth_provider.dart';
 import 'onboarding_screen.dart';
+import 'login_screen.dart';
+import '../home/home_screen.dart';
 
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
